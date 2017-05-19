5f6c711026cac7d692749f92ca399fe8002e75d7
Forrest Desjardins <desjardinsfg@gmail.com>
2017-05-19 03:13:16 -0800

    add multiprocessing dropout SGD example

--- 06_keras/multiprocessing-dropout-sgd.py
+++ 06_keras/multiprocessing-dropout-sgd.py
@@ -3,7 +3,6 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.constraints import maxnorm
-from keras.optimizers import SGD
 import numpy
 from sklearn.model_selection import StratifiedKFold
 import redis
@@ -30,13 +29,11 @@ def trainCycle(train, test, i):
     print('starting training cycle %i/10...' % i)
     kf_model = Sequential()
     kf_model.add(Dropout(0.2, input_shape=(8,)))
-    kf_model.add(Dense(60, input_dim=8, activation='relu', kernel_initializer='normal', kernel_constraint=maxnorm(3)))
-    kf_model.add(Dropout(0.2, input_shape=(30,)))
-    kf_model.add(Dense(30, activation='relu', kernel_initializer='normal', kernel_constraint=maxnorm(3)))
+    kf_model.add(Dense(12, input_dim=8, activation='relu', kernel_initializer='normal', kernel_constraint=maxnorm(3)))
+    kf_model.add(Dense(8, activation='relu', kernel_initializer='normal', kernel_constraint=maxnorm(3)))
     kf_model.add(Dense(1, activation='sigmoid'))
-    sgd = SGD(lr=0.1, momentum=0.9, decay=0.0, nesterov=False)
-    kf_model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
-    kf_model.fit(X[train], Y[train], validation_data=(X[test], Y[test]), epochs=600, batch_size=32, verbose=0)
+    kf_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    kf_model.fit(X[train], Y[train], validation_data=(X[test], Y[test]), epochs=150, batch_size=10, verbose=0)
     scores = kf_model.evaluate(X[test], Y[test], verbose=0)
     r.rpush("tensorflow:multiprocessing:results", scores[1] * 100)
 
